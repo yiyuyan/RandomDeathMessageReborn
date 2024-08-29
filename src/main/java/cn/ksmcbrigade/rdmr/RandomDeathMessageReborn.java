32c899f0bc4b5fc55be8ae3feb3d2d023b8f9d9a
yiyuyan <yiyuyan>
2024-08-29 19:36:45 +0800

    Initial commit

---
+++ src/main/java/cn/ksmcbrigade/rdmr/RandomDeathMessageReborn.java
@@ -0,0 +1,46 @@
+package cn.ksmcbrigade.rdmr;
+
+import net.minecraftforge.fml.common.Mod;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+@Mod("rdmr")
+public class RandomDeathMessageReborn {
+    public static final URL REMOTE_URL;
+
+    static {
+        try {
+            REMOTE_URL = new URL("https://raw.githubusercontent.com/yiyuyan/RandomDeathMessageReborn/master/src/main/resources/messages.txt");
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static final List<String> MESSAGES = initRDMR();
+
+    public static List<String> initRDMR() {
+
+        try {
+            List<String> returnList = new ArrayList<>();
+            Scanner s = new Scanner(REMOTE_URL.openStream());
+            while (s.hasNextLine()) returnList.add(s.nextLine());
+            return returnList;
+        } catch (IOException e) {
+            System.out.println("Remote messages failure. Use fallback.");
+
+            try {
+                return Files.readAllLines(Path.of(RandomDeathMessageReborn.class.getResource("/messages.txt").getPath()));
+            } catch (IOException ex) {
+                System.out.println("RDM Total Failiure.");
+                throw new RuntimeException(ex);
+            }
+        }
+    }
+}
---
+++ src/main/java/cn/ksmcbrigade/rdmr/mixin/DamageSourceMixin.java
@@ -0,0 +1,23 @@
+package cn.ksmcbrigade.rdmr.mixin;
+
+import cn.ksmcbrigade.rdmr.RandomDeathMessageReborn;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.LivingEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+@Mixin(DamageSource.class)
+public class DamageSourceMixin {
+    /**
+     * @author KSmc_brigade
+     * @reason get the custom death message
+     */
+    @Overwrite
+    public Component getLocalizedDeathMessage(LivingEntity entity) {
+        String message = RandomDeathMessageReborn.MESSAGES.get(ThreadLocalRandom.current().nextInt(0, RandomDeathMessageReborn.MESSAGES.size())).replaceAll("<rdm[.]player[.]name>",entity.getDisplayName().getString());
+        return Component.literal(message);
+    }
+}
